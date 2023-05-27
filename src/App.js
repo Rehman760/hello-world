c91ce40c3bf06d422cad7ee5e2aae294b6aee03f
Abdul Rehman <abdulrehmanq59@gmail.com>
2023-05-27 17:22:19 +0500

    add Thank you Message component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 import AnotherUserParticipation from './components/AnotherUserParticipation';
 import PollDisplay from './components/PollDisplay';
+import ThankYouMessage from './components/ThankYouMessage';
 import UserParticipation from './components/UserParticipation';
 function App() {
   return (
     <div className="App">
       <h1>Poll App</h1>
-      {/* <UserParticipation render={(question,choices,selectedOption,handleOptionChange,handleVote)=>
+      <UserParticipation render={(question,choices,selectedOption,handleOptionChange,handleVote)=>
       <PollDisplay question={question} choices={choices} selectedOption={selectedOption}
-      handleOptionChange={handleOptionChange} handleVote={handleVote}/>}/> */}
-     
+      handleOptionChange={handleOptionChange} handleVote={handleVote}/>}/>
      <AnotherUserParticipation/>
+     <ThankYouMessage/>
     </div>
   );
 }
---
+++ src/components/ThankYouMessage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ThankYouMessage() {
+  return (
+    <div>ThankYouMessage</div>
+  )
+}
+
+export default ThankYouMessage
